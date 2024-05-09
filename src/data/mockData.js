863316d2f788423fbb6ddabf4dcf5e383081004c
Muhammad Rana <muhammadrana523@gamil.com>
2024-05-09 16:39:00 +0500

    Sidbar and header pushed

---
+++ src/data/mockData.js
@@ -0,0 +1,58 @@
+export const mockDataTeam = [
+    {
+        id: 1,
+        name: 'Ben Manager',
+        eamil: 'benmanager@gmial.com',
+        age: 35,
+        phone: "(665)121-5454",
+        access: "manager",
+    },
+    {
+        id: 2,
+        name: 'Ben Project Manager',
+        eamil: 'benprojectmanager@gmial.com',
+        age: 35,
+        phone: "(665)121-5353",
+        access: "Project Manager",
+    },
+    {
+        id: 3,
+        name: 'Ben Account Manager',
+        eamil: 'benaccountmanager@gmial.com',
+        age: 38,
+        phone: "(665)121-5252",
+        access: "Account Manager",
+    },
+    {
+        id: 4,
+        name: 'Ben Video Designer',
+        eamil: 'benvideodesigner@gmial.com',
+        age: 25,
+        phone: "(665)123-5151",
+        access: "Video Designer",
+    },
+    {
+        id: 5,
+        name: 'Ben Graphic Specialist',
+        eamil: 'bengraphicspecialist@gmial.com',
+        age: 27,
+        phone: "(665)141-3232",
+        access: "Graphic Specialist",
+    },
+    {
+        id: 6,
+        name: 'Ben Project Cordinator',
+        eamil: 'benprojectcordinator@gmial.com',
+        age: 42,
+        phone: "(665)130-2323",
+        access: "Cordinator",
+    },
+    {
+        id: 7,
+        name: 'Ben Podcast Host',
+        eamil: 'benpodcasthost@gmial.com',
+        age: 28,
+        phone: "(665)126-1122",
+        access: "Host",
+    }
+]
