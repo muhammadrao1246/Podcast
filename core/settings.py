4607dd15d8a6177d479067380e1eb87f67d9bc10
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-06-04 12:43:25 +0500

    re-commit

--- core/settings.py
+++ core/settings.py
@@ -251,14 +251,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Adjusted path for media files
 
-# cpanel
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Adjusted path for media files
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
