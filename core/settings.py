6e2f7d7fb77513652d55e948d286d469b6686508
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-06-16 20:07:26 +0500

    Logging Enabled:

--- core/settings.py
+++ core/settings.py
@@ -172,24 +172,24 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 # LOGIN_REDIRECT_URL = "/dashboard"
 # LOGOUT_REDIRECT_URL = "/login"
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'file': {
-#             'level': 'DEBUG',
-#             'class': 'logging.FileHandler',
-#             'filename': os.path.join(BASE_DIR, "/debug.log"),
-#         },
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['file'],
-#             'level': 'DEBUG',
-#             'propagate': True,
-#         },
-#     },
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, "/debug.log"),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 
 
