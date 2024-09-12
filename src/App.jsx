317687bc629ed9ad7c4469707a0bc92a3e788efa
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-09-13 02:27:06 +0500

    Env Updated

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,13 @@ import {ROUTES} from 'src/routes';
 
 import "src/assets/css/index.css"
 
-import MainLayout from './components/Layouts/MainLayout';
-import AuthLayout from './components/Layouts/AuthLayout';
-import Login from './pages/auth/Login';
-import Signup from './pages/auth/Signup';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import ResetPassword from './pages/auth/ResetPassword';
-import { PrivateRoute, PublicRoute } from './helpers/protectors';
+import MainLayout from 'src/components/Layouts/MainLayout';
+import AuthLayout from 'src/components/Layouts/AuthLayout';
+import Login from 'src/pages/auth/Login';
+import Signup from 'src/pages/auth/Signup';
+import ForgotPassword from 'src/pages/auth/ForgotPassword';
+import ResetPassword from 'src/pages/auth/ResetPassword';
+import { PrivateRoute, PublicRoute } from 'src/helpers/protectors';
 
 
 // all pages components are declared here
