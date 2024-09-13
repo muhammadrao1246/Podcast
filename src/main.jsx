ba540cf4438edddb9e54d381f59478008d36921b
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-09-13 15:47:01 +0500

    Reversed Project Package Files to a success commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import {PersistGate} from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store} >
       <PersistGate loading={null} persistor={persistor}>
         <SnackbarProvider maxSnack={3}>
@@ -20,5 +20,5 @@ root.render(
         </SnackbarProvider>
       </PersistGate>
     </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
