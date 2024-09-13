76c1eab5b301bf61c55a83e72153e6326a3d7296
Muhammad Bin Zulfiqar <muhammadrao1246@gmail.com>
2024-09-13 18:10:33 +0500

    Redeploy

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,9 @@ export default defineConfig({
     open: true,
     // this sets a default port to 3000  
     port: 3000, 
-    headers: {
-      "Cross-Origin-Embedder-Policy": "unsafe-none"
-    }
+    // headers: {
+    //   "Cross-Origin-Embedder-Policy": "unsafe-none"
+    // }
   },
   resolve:{
     alias: {
